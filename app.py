aa576c6fcbfece9d5b45e1264e69a5002d150bf0
jarshamdhir <jarshamdhir199912@gmail.com>
2023-11-10 18:55:49 -0500

    add marker

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import folium
 map = folium.Map(location=[38.58, -99.09],
-                 zoom_start=6)
+                 zoom_start=6, tiles="OpenStreetMap")
+map.add_child(folium.Marker(location=[
+              38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 map.save("map2.html")
